0c7c348f957821482aa450448e6c879b2e20522e
leonelborgesdev <leonel03t@gmail.com>
2023-10-19 11:23:11 -0400

    create client

--- src/controllers/cliente.controller.js
+++ src/controllers/cliente.controller.js
@@ -1,8 +1,15 @@
+import { Cliente } from "../models/Cliente.js";
+
 export const getClients = (req, res) => {
   res.send("getting clients");
 };
 
-export const createClient = (req, res) => {
-  //   console.log(req.body);
-  res.send("create client");
+export const createClient = async (req, res) => {
+  const { id, nombre, celular } = req.body;
+  try {
+    const newClient = await Cliente.create({ id, nombre, celular });
+    return res.status(200).json({ newClient });
+  } catch (error) {
+    return res.status(400).json({ msg: "Consulte con su administrador" });
+  }
 };
