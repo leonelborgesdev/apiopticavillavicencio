cfcd5544ecde49e31538e615abce18f7cbfff3d7
leonelborgesdev <leonel03t@gmail.com>
2023-10-23 08:44:05 -0400

    en produccion recetas

--- src/database/database.js
+++ src/database/database.js
@@ -4,15 +4,15 @@ config();
 
 const { DATABASE_URL, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 export const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/opticadb`,
-  //DATABASE_URL,
+  // `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/opticadb`,
+  DATABASE_URL,
   {
-    //---------------------------Deploy--------------------------
-    // dialectOptions: {
-    //   ssl: { require: true, rejectUnauthorized: false },
-    // },
-    //------------------------------------------------------------
-    // logging: false, // set to console.log to see the raw SQL queries
-    // native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    // ---------------------------Deploy--------------------------
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+    // ------------------------------------------------------------
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
 );
